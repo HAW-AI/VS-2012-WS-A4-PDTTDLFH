c4c442a2b668f1aae9f5bcede637239da8c395f5
Till Theis <theis.till@gmail.com>
2013-01-04 20:36:27 +0100

    style

--- utility.erl
+++ utility.erl
@@ -9,14 +9,14 @@ current_timestamp() ->
   {Mega, Sec, Micro} = now(),
   Mega * 1000000000 + Sec * 1000 + Micro div 1000.
 
-current_frame()->
-	{_,CurrentFrame,_}=erlang:now(),
+current_frame() ->
+	{_, CurrentFrame, _} = now(),
 	CurrentFrame.
 
  % Timestamp rem 1000 => ms part of timestamp
  % ms part / 50 => the slot cause 1000 ms / 50 => 20 slots
-slot_of_timestamp(Timestamp)->
-	erlang:trunc(((Timestamp rem 1000)/50)).
+slot_of_timestamp(Timestamp) ->
+	trunc(((Timestamp rem 1000) / 50)).
 
 % time in milliseconds until slot in _next_ frame
 time_until_slot(Slot) ->
