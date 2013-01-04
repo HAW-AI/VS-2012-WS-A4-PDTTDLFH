bf510b3aa4d6ef4e2681ee7779e11daab324c8d5
Daniel <no@reply.com>
2013-01-04 15:34:13 +0100

    implemented get_data and input case, added some io:format

--- datasource.erl
+++ datasource.erl
@@ -13,38 +13,47 @@
          terminate/2,
          code_change/3]).
 
--record(state, {input_reader_pid :: pid()}).
+-record(state, {input_reader_pid :: pid(), current_data=[]}).
 
 start() ->
   gen_server:start(?MODULE, [], []).
 
 init([]) ->
   %%% start reading for the input source
+  io:format("datasource init"),
   InputReaderPID = spawn(fun() -> read_from_datasource(self()) end),
 
   {ok, #state{input_reader_pid = InputReaderPID}}.
 
 handle_cast({input, Data}, State) ->
-  %%% TODO handle input in here somehow
-  {noreply, State};
+  io:format("new data stored: ~p~n",[Data]),
+  {noreply, State#state{current_data=Data}};
 
+handle_cast({get_data, PID}, State) ->
+  io:format("data transfered to sender: ~p~n",[State#state.current_data]),
+  gen_fsm:send_event(PID,{input, State#state.current_data}),
+  {noreply, State#state{current_data=[]}};
+  
 handle_cast(kill, State) ->
-  {stop, normal, State}.
+  io:format("datasource killed"),
+  {stop, normal, State};
 
+handle_cast(Any, State) ->
+  io:format("received unknown msg: ~p~n",[Any]),
+  {noreply, State}.
+  
 %%% do everything required for a clean shutdown
 terminate(_Reason, State) ->
   exit(State#state.input_reader_pid, normal),
   ok.
 
-
-
-
-
 read_from_datasource(DatasourcePID) ->
   case io:get_chars("", 24) of
     %%% TODO log something
-    eof -> exit(normal);
-    {error, Reason} -> exit(Reason);
+    eof ->
+		exit(normal);
+    {error, Reason} ->
+		exit(Reason);
     NextChunk ->
       gen_server:cast(DatasourcePID, {input, NextChunk}),
       read_from_datasource(DatasourcePID)
@@ -57,6 +66,7 @@ handle_call(_Request, _From, State) ->
   {reply, Reply, State}.
 
 handle_info(_Info, State) ->
+  io:format("how about sending gen server a msg in a proper way"),
   {noreply, State}.
 
 code_change(_OldVsn, State, _Extra) ->
