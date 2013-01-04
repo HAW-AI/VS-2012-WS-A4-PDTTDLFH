d0db2ad5f456b028a7be5f197d1ecc2b392b3623
Daniel <no@reply.com>
2013-01-04 17:22:30 +0100

    added -slot and timestamp calculation -some io:format

--- receiver.erl
+++ receiver.erl
@@ -21,35 +21,35 @@ start(CoordinatorPID, ReceivingSocket) ->
 
 init([CoordinatorPID, ReceivingSocket]) ->
   gen_udp:controlling_process(ReceivingSocket, self()),
-
-  {ok, #state{coordinator_pid  = CoordinatorPID,
-              receiving_socket = ReceivingSocket}}.
+  {ok, #state{coordinator_pid  = CoordinatorPID, receiving_socket = ReceivingSocket}}.
 
 handle_cast({udp, _Socket, _IP, _InPortNo, Packet}, State) ->
-  Slot = 1234567890, % FIXME
-  Time = foo,        % FIXME
-
-  gen_server:cast(State#state.coordinator_pid,
-                  {received, Slot, Time, Packet}),
+  io:format("received packet"),
+  Timestamp = utility:current_timestamp(),
+  Slot = utility:slot_of_timestamp(Timestamp),
+  gen_server:cast(State#state.coordinator_pid, {received, Slot, Timestamp, Packet}),
   {noreply, State};
 
 handle_cast(kill, State) ->
+  io:format("receiver killed"),
   {stop, normal, State}. % calls :terminate and then shuts down
-
+  
+handle_cast(Any, State) ->
+  io:format("received unknown msg: ~p~n",[Any]),
+  {noreply, State}.
+  
 %%% do everything required for a clean shutdown
 terminate(_Reason, State) ->
   gen_udp:close(State#state.receiving_socket),
   ok.
-
-
-
-
+			
 %%% OTP gen_server boilerplate - ignore this
 handle_call(_Request, _From, State) ->
   Reply = ok,
   {reply, Reply, State}.
 
 handle_info(_Info, State) ->
+  io:format("how about sending gen server a msg in a proper way"),
   {noreply, State}.
 
 code_change(_OldVsn, State, _Extra) ->
