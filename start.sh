122e25c8ee9f327b92a087e88dc811e8688e0f71
Till Theis <theis.till@gmail.com>
2013-01-09 14:42:26 +0100

    allow setting sending port in start script

--- start.sh
+++ start.sh
@@ -2,8 +2,10 @@
 
 if [ "$#" == "5" ] ; then
   erl -sname sender$3 -setcookie hallo -boot start_sasl -noshell -s coordinator start $@
+elif [ "$#" == "6" ] ; then
+  erl -sname sender$4 -setcookie hallo -boot start_sasl -noshell -s coordinator start $@
 else
-  echo "usage: ./start.sh <ReceivingPort> <TeamNumber> <StationNumber> <MulticastIP> <LocalIP>"
+  echo "usage: ./start.sh <ReceivingPort> [<SendingPort>] <TeamNumber> <StationNumber> <MulticastIP> <LocalIP>"
   echo "example: ./start.sh 1338 08 99 225.10.1.2 127.0.0.1"
   echo "big example: java -cp ~/Downloads/datasource/ datasource.DataSource 6 99 | ./start.sh 1338 08 99 225.10.1.2 127.0.0.1"
 fi
