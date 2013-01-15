dd38b9d01a365ee27ab97c79cb21791e00d57cce
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2013-01-15 20:34:08 +0100

    start.sh now deletes old beam files and recompiles

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 cp /dev/null log/all.log
+rm *.beam
+echo "re-compiling"
+erlc *.erl
 
 for n in `seq $2 $3`
 do
-(java datasource.DataSource $n $n | erl +A25 -sname sender$n -setcookie vsp -boot start_sasl -noshell -s coordinator start 1337 $n $n 225.10.1.2 $1) >> log/all.log &
-sleep 0.2
+  (java datasource.DataSource $n $n | erl +A25 -sname sender$n -setcookie vsp -boot start_sasl -noshell -s coordinator start 1337 $n $n 225.10.1.2 $1) >> log/all.log &
+  sleep 0.2
 done
