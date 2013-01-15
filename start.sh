ba43774bb212ab164b635aa1a941db6ad4af2d10
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2013-01-15 21:21:40 +0100

    more flexible start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,14 @@ rm *.beam
 echo "re-compiling"
 erlc *.erl
 
+#$1 adresse des zu verwendenen Interfaces
+#$2 start-client-nummer
+#$3 end-client-nummer
+#$4 Port
+#$5 Multicast Adresse
 for n in `seq $2 $3`
 do
-  (java datasource.DataSource $n $n | erl +A25 -sname sender$n -setcookie vsp -boot start_sasl -noshell -s coordinator start 1337 $n $n 225.10.1.2 $1) >> log/all.log &
+  echo "starting client-$n"
+  (java datasource.DataSource $n $n | erl +A25 -sname sender$n -setcookie vsp -boot start_sasl -noshell -s coordinator start $4 $n $n $5 $1) >> log/all.log &
   sleep 0.2
 done
